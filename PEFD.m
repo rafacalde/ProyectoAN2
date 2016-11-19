e9681654523477175a873bd9d1cf7a6beec800a9
rafacalde <rafacalde7@gmail.com>
2016-11-18 23:16:39 -0600

    Update PEFD.m

--- PEFD.m
+++ PEFD.m
@@ -1,4 +1,5 @@
 
+
 % Nombres: -Rafael Aníbal Calderón Melara  CM11099
 %          -Estely Dolores Méndez Moreno   MM09204
 
@@ -12,19 +13,19 @@
 % y a su vez el método de Gauss Seidel.
 % Notar que se utiliza la norma infinita en nuestro código, 
 % pero se podría utilizar cualquier otra norma.
-% INPUT:  -- f,g: Funciones
+% INPUT:  -- f: Funciones
 %        -- a,b,c,d: Valores frontera.
 %        -- n>=3, m>=3: Enteros que sirven para determinar las 
 %                   dimensiones de la cuadrícula.
 %        -- tol: Tolerancia  requerida.
 %        -- N: Máximo número de iteraciones.
 
-% OUTPUT: -- w: aproximaciones para u(x,y) que es la solución
-%              de la ecuación diferencial de segundo orden.
-%        -- l: número de iteraciones requeridas para obtener 
+% OUTPUT: -- Tabla con las aproximaciones para u(x,y) y los puntos 
+%            x & y.
+%         -- l: número de iteraciones requeridas para obtener 
 %              la solución.
 
-function [] = PEFD(f,g,a,b,c,d,m,n,tol,N)
+function [] = PEFD(f,a,b,c,d,m,n,tol,N)
 
 % h y k representan el tamaño de paso para la construcción de los
 % puntos x_i e y_j respectivamente.
@@ -58,14 +59,14 @@ l = 1;
 while l <= N 
     
     % Aquí se actualiza el elemento 1 de la última columna de w.
-    z = (-(h^2)*f(x(1),y(m-1))+g(a,y(m-1))+lambda*g(x(1),d)+lambda*w(1,m-2)+w(2,m-1))/mu;
+    z = (-(h^2)*f(x(1),y(m-1))+fung(a,y(m-1))+lambda*fung(x(1),d)+lambda*w(1,m-2)+w(2,m-1))/mu;
     NORM = norm(z-w(1,m-1),inf);
     w(1,m-1)=z;
     
     % El siguiente for actualiza los valores de w, desde el segundo
     % al penúltimo elemento de w, de la última columna.
     for i = 2:n-2
-        z = (-(h^2)*f(x(i),y(m-1))+lambda*g(x(i),d)+w(i-1,m-1)+w(i+1,m-1)+lambda*w(i,m-2))/mu;
+        z = (-(h^2)*f(x(i),y(m-1))+lambda*fung(x(i),d)+w(i-1,m-1)+w(i+1,m-1)+lambda*w(i,m-2))/mu;
         if norm(w(i,m-1)-z,inf) > NORM
             NORM = norm(w(i,m-1)-z,inf);
         end
@@ -73,7 +74,7 @@ while l <= N
     end
     
     % Aquí se actualiza el último valor de la última columna de w.
-    z = (-(h^2)*f(x(n-1),y(m-1))+g(b,y(m-1))+lambda*g(x(n-1),d)+w(n-2,m-1)+lambda*w(n-1,m-2))/mu;
+    z = (-(h^2)*f(x(n-1),y(m-1))+fung(b,y(m-1))+lambda*fung(x(n-1),d)+w(n-2,m-1)+lambda*w(n-1,m-2))/mu;
     if norm(w(n-1,m-1)-z,inf) > NORM
         NORM = norm(w(n-1,m-1)-z,inf);
     end
@@ -85,7 +86,7 @@ while l <= N
         
         % Aquí actualizan los valores de w, desde el segundo al penúltimo, 
         % de la primera fila.
-        z = (-(h^2)*f(x(1),y(j))+g(a,y(j))+lambda*w(1,j+1)+lambda*w(1,j-1)+w(2,j))/mu;
+        z = (-(h^2)*f(x(1),y(j))+fung(a,y(j))+lambda*w(1,j+1)+lambda*w(1,j-1)+w(2,j))/mu;
         if norm(w(1,j)-z,inf) > NORM
             NORM = norm(w(1,j)-z,inf);
         end
@@ -103,7 +104,7 @@ while l <= N
         
         % Aquí se actualizan los valores de w, desde el segundo al penúltimo,
         % de la última fila
-        z = (-(h^2)*f(x(n-1),y(j))+g(b,y(j))+w(n-2,j)+lambda*w(n-1,j+1)+lambda*w(n-1,j-1))/mu;
+        z = (-(h^2)*f(x(n-1),y(j))+fung(b,y(j))+w(n-2,j)+lambda*w(n-1,j+1)+lambda*w(n-1,j-1))/mu;
         if norm(w(n-1,j)-z,inf) > NORM
             NORM = norm(w(n-1,j)-z,inf);
         end
@@ -111,7 +112,7 @@ while l <= N
     end
     
     % Aquí se actualiza el primer valor de la primera fila y primera columna de w. fila 1 y columna 1 de w.
-    z = (-(h^2)*f(x(1),y(1))+g(a,y(1))+lambda*g(x(1),c)+lambda*w(1,2)+w(2,1))/mu;
+    z = (-(h^2)*f(x(1),y(1))+fung(a,y(1))+lambda*fung(x(1),c)+lambda*w(1,2)+w(2,1))/mu;
     if norm(w(1,1)-z,inf) > NORM
         NORM = norm(w(1,1)-z,inf);
     end
@@ -120,7 +121,7 @@ while l <= N
     % El siguiente for actualiza los valores de w, desde el segundo al penúltimo
     % de la primera columna.
     for i = 2:n-2
-        z = (-(h^2)*f(x(i),y(1))+lambda*g(x(i),c)+w(i-1,1)+lambda*w(i,2)+w(i+1,1))/mu;
+        z = (-(h^2)*f(x(i),y(1))+lambda*fung(x(i),c)+w(i-1,1)+lambda*w(i,2)+w(i+1,1))/mu;
         if norm(w(i,1)-z,inf) > NORM
             NORM = norm(w(i,1)-z,inf);
         end    
@@ -128,7 +129,7 @@ while l <= N
     end
     
     % Aquí se actualiza el último valor de la ultima columna de la matriz w.
-    z = (-(h^2)*f(x(n-1),y(1))+g(b,y(1))+lambda*g(x(n-1),c)+w(n-2,1)+lambda*w(n-1,2))/mu;
+    z = (-(h^2)*f(x(n-1),y(1))+fung(b,y(1))+lambda*fung(x(n-1),c)+w(n-2,1)+lambda*w(n-1,2))/mu;
     if norm(w(n-1,1)-z,inf) > NORM
         NORM = norm(w(n-1,1)-z,inf);
     end
@@ -151,7 +152,7 @@ end
 I=zeros(m-1,1);
 J=zeros(m-1,1);
 %imprime los títulos de cada columna de la tabla resultante
-fprintf('      i         j         xi        xj      w(i,j) \n')
+fprintf('      i         j         xi          xj          w(i,j) \n')
 for j=1:m-1 %se llena los valores para los índices j en la tabla resultante
     J(j)=j;
 end
@@ -161,8 +162,9 @@ for i=1:n-1 %se llenan los vectores con las coordenadas de x e y
         I(j)=i; %se llena los valores para los indices i en la tabla resultante
         x1(j)=x(i);
     end
+    A= [ I  J  x1 transpose(y) transpose(w(i,:)) ];
     %imprime los elemento de la tabla
-    disp([ I  J  x1 transpose(y) transpose(w(i,:)) ])
+    fprintf([repmat('%f\t', 1, size(A, 2)) '\n'], A');
 end
 % muestra el número de iteraciones
 fprintf('\n Y el numero de iteraciones necesarias fue: %d ', l) 
@@ -170,7 +172,6 @@ end % termina el codigo
 
 % Ejecución del ejemplo 2 , de la sección 12.1 del libro de Burden
 % f = @(x,y)x*exp(y);
-% g = @(x,y)x*exp(y);
 % a=0; b=2; c=0; d=1; m=5; n=6; tol=1e-10; N=100; 
-% PEFD(f,g,a,b,c,d,m,n,tol,N)
+% PEFD(f,a,b,c,d,m,n,tol,N)
 
